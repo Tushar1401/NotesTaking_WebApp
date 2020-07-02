b042c0afba3a37b86e78520dc5e0ad159628b600
Tushar1401 <tushararora142001@gmail.com>
2020-07-02 22:57:28 +0530

    added title field

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ showNotes();
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -12,9 +13,14 @@ addBtn.addEventListener("click", function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = " ";
+    addTitle.value = " ";
     // console.log(notesObj);
     showNotes();
 });
@@ -33,8 +39,8 @@ function showNotes() {
         html += ` 
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
           <div class="card-body">
-              <h5 class="card-title">Note ${index + 1}</h5>
-              <p class="card-text">${element}</p>
+              <h5 class="card-title">${element.title}</h5>
+              <p class="card-text">${element.text}</p>
               <button id="${index}" onClick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
           </div>
         </div>
